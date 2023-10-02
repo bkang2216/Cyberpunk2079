1d5ca4dfdb03fba7fedc583a21704d0d78c7c73f
GTC-Brayden-A <braydenaallenbaa@gmail.com>
2023-10-02 17:53:26 -0400

    Player Control
    - Changed player script

--- My project/Assets/Scripts/Player/PlayerControl.cs
+++ My project/Assets/Scripts/Player/PlayerControl.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    public float movementSpeed = 5f;
+
+    float inputH;
+    float inputV;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputH = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2 ( Time.deltaTime * movementSpeed * inputH, 0);
+        rb.MovePosition(movement);
     }
 }
