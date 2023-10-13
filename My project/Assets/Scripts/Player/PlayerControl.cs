cdfab49d38b700297cf95f9e205056f9d68b28be
GTC-Brayden-A <braydenaallenbaa@gmail.com>
2023-10-12 21:22:40 -0400

    Player Update
    - Added double jump
    - Rewrote some of the code for better controls

--- My project/Assets/Scripts/Player/PlayerControl.cs
+++ My project/Assets/Scripts/Player/PlayerControl.cs
@@ -12,7 +12,9 @@ public class PlayerControl : MonoBehaviour
     public float jumpPower = 10f;
 
     float inputH;
-    float inputJump;
+    float jump = 0;
+
+    const float MAXJUMP = 2;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,12 +23,18 @@ public class PlayerControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         inputH = Input.GetAxisRaw("Horizontal");    // Get input from A/D keys
-        inputJump = Input.GetAxisRaw("Jump");       // Get input from Spacebar key
 
         Vector2 movement = inputH * movementPower * Time.deltaTime * Vector2.right; // Calculate the movement for the player
+
+        // Limits the player's jump to only allow it when not exceeding the max jump limit
+        if (Input.GetButtonDown("Jump") && jump != MAXJUMP)
+        {
+            rb.velocity = Vector2.up * jumpPower;   //Adds vertical force to the player
+            ++jump;
+        }
 
         // Limits the player's velocity in both directions
         if (rb.velocity.x <= movementPower && rb.velocity.x >= -movementPower)
@@ -34,17 +42,13 @@ public class PlayerControl : MonoBehaviour
             rb.AddForce(movement, ForceMode2D.Impulse); // Adds horizontal force to the player
         }
         
-        // Limits the player's jump to only allow it once grounded
-        if (inputJump == 1 && rb.velocity.y == 0)
-        {
-            rb.velocity = Vector2.up * jumpPower;   //Adds vertical force to the player
-        }
-
+        
         // Once the player stops pressing A or D, they will come to a immediate stop
         if (inputH == 0 && rb.velocity.x != 0 && rb.velocity.y == 0)
         {
             rb.velocity = Vector2.right * 0;    // Puts the player's velocity to 0
         }
+        
+        if (rb.velocity.y == 0) { jump = 0; } // Resets player's jumps to 0 after retaining a 0 velocity on the y-axis
     }
-
 }
